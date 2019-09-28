fe6c13c3908018eabb38bfa08757ef41b80de12a
王文腾 <120591516@qq.com>
2019-09-28 17:09:10 +0800

    添加druid数据源

---
+++ src/main/java/com/mooc/house/config/DruidConfig.java
@@ -0,0 +1,37 @@
+package com.mooc.house.config;
+
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.google.common.collect.Lists;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DruidConfig {
+    @ConfigurationProperties(prefix = "spring.druid")
+    @Bean(initMethod = "init",destroyMethod = "close")
+    public DruidDataSource dataSource(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setProxyFilters(Lists.newArrayList(statFilter()));
+        return druidDataSource;
+    }
+
+    @Bean
+    public Filter statFilter(){
+        StatFilter statFilter = new StatFilter();
+        statFilter.setLogSlowSql(true);
+        statFilter.setSlowSqlMillis(5000);
+        statFilter.setMergeSql(true);
+        return statFilter;
+    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        return new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+    }
+
+}
